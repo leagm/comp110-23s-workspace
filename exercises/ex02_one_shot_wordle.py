98239f1a63638fd8a64a8be45c8ff4438bda5e0e
Leah Morrissey <lmorrissey@unc.edu>
2023-02-06 22:28:32 -0500

    Almost finished EX02!

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,14 +4,13 @@ __author__ = "730486771"
 
 SECRET_WORD: str = "python"
 guess: str = input(f"What is your {len(SECRET_WORD)}-letter guess? ")
+# 
 playing: bool = True
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 count_idx: int = 0
 emoji: str = ""
-elsewhere: bool = False
-match_idx: int = 0
 
 while playing:
     if (len(guess) != len(SECRET_WORD)):
@@ -20,16 +19,24 @@ while playing:
         
     else:
         while count_idx < len(SECRET_WORD):
-            while match_idx < len(SECRET_WORD) and elsewhere:
-                if (guess[count_idx] == SECRET_WORD[match_idx]):
-                    elsewhere = True
-                else:
-                    match_idx = match_idx + 1
             if (guess[count_idx] == SECRET_WORD[count_idx]):
                 emoji = emoji + GREEN_BOX
+                count_idx =  count_idx + 1
             else:
-                emoji = emoji + WHITE_BOX
-            count_idx =  count_idx + 1
+                match_idx = int = 0
+                elsewhere = False
+                while elsewhere == False and match_idx < len(SECRET_WORD):
+                    if (guess[count_idx] == SECRET_WORD[match_idx]):
+                        elsewhere = True
+                    else:
+                        match_idx = match_idx + 1
+                if elsewhere == True:
+                    emoji = emoji + YELLOW_BOX
+                    count_idx = count_idx + 1
+                else:
+                    emoji = emoji + WHITE_BOX
+                    count_idx = count_idx + 1
+            emoji = emoji
         print(emoji)
         if (guess == SECRET_WORD):
             print("Woo! You got it! ")
