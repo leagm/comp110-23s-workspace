16ed34b91fe1cb34d1b116ffdaa2fa664e53df61
Leah Morrissey <lmorrissey@unc.edu>
2023-02-07 11:35:51 -0500

    Finished EX02 One Shot Wordle

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -24,27 +24,34 @@ while playing:
             if (guess[count_idx] == SECRET_WORD[count_idx]):
                 emoji = emoji + GREEN_BOX
                 count_idx =  count_idx + 1
-# Lines 28-30 continue the code if an index in the guess does not match the corresponding index in the secret word (in other words, if the index comparison doesn't result in a green box)
+# Lines 28-30 continue the code if the indices do not evaluate to a green box. Match_idx is reset to 0 so that the following while loop will compare the guess index to all indices of the secret word. The bool elsewhere is set to False to keep the following while loop running
             else:
                 match_idx = int = 0
                 elsewhere = False
-# 
+# Lines 32-34 start an embedded while loop which will compare each index of the guess word to every index of the secret word as long as the bool elsewhere remains False
                 while elsewhere == False and match_idx < len(SECRET_WORD):
                     if (guess[count_idx] == SECRET_WORD[match_idx]):
                         elsewhere = True
+# Lines 36-37 tell the program to increase the index counter for the secret word (match_idx) if the indices of the secret word do not match with that of the guess word.
                     else:
                         match_idx = match_idx + 1
+# Lines 39-41 provide a further direction for once the if and else statements above have been evaluated. If elsewhere was assigned to be True above, then a yellow box emoji should be added to the emoji string at the appropriate index, count_idx should be increased and then the while loop should start over with a new index in the guess to be compared to all indices of the secret word
                 if elsewhere == True:
                     emoji = emoji + YELLOW_BOX
                     count_idx = count_idx + 1
+# Lines 43-45 tell the program that if there were no matches for an index in the guess word with any of the indices in the secret word then a white box emoji should be added to the appropriate index in the emoji string
                 else:
                     emoji = emoji + WHITE_BOX
                     count_idx = count_idx + 1
+# Lines 47-48 finalize the placement of the emoji boxes within the emoji string before printing the string result
             emoji = emoji
         print(emoji)
+# Lines 50-52 print the message "Woo! You got it! " if the user correctly guessed the secret word. The bool playing will be assigned False in this instance because the game has been won and playing is no longer True
         if (guess == SECRET_WORD):
             print("Woo! You got it! ")
             playing = False
+# Lines 54-56 print the message "Not quite. Play again soon!  " if the user's inputted guess was not exactly equal to the secret word. The bool playing is assigned false in this instance because the one guess has been fully evaluated and was not correct so the game is over and playing is no longer True
         else:
-            print("Not quite, Play again soon! ")
+            print("Not quite. Play again soon! ")
             playing = False
+# Finished the code! :)
