978a272aec9a7864855e6c872bad57990e391917
Leah Morrissey <lmorrissey@unc.edu>
2023-02-20 21:10:27 -0500

    Almost done with EX03!

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -6,13 +6,13 @@ def contains_char(search_through: str, search_for: str) -> bool:
     """When given two strings, the first of any length, and the second a single character, return True if the second string is found anywhere in the first and return False if otherwise"""
     assert len(search_for) == 1
     count_idx: int = 0
-    while count_idx < len(search_through):
+    elsewhere = False
+    while elsewhere == False and count_idx < len(search_through):
         if (search_through[count_idx] == search_for):
-            return True
+            elsewhere = True
         else:
             count_idx = count_idx + 1
-            if (search_through != search_for):
-                return False
+    return elsewhere
 
 def emojified(guess_word: str, secret_word: str) -> str:
     """Given two strings of equal length, one which is a guess and the other which is a secret, return a string of emojis that will codify according to the placement/existence of characters within guess words"""
@@ -27,13 +27,41 @@ def emojified(guess_word: str, secret_word: str) -> str:
             emoji = emoji + GREEN_BOX
             counter_idx = counter_idx + 1
         else:
-            elsewhere = False
-            while elsewhere is False:
-                if (contains_char(secret_word, guess_word[counter_idx]) == True):
-                    emoji = emoji + YELLOW_BOX
-                    counter_idx = counter_idx + 1
-                else:
-                    emoji = emoji + WHITE_BOX
-                    counter_idx = counter_idx + 1
-        emoji = emoji
-    print(emoji)
+            if contains_char(secret_word, guess_word[counter_idx]) == True:
+                emoji = emoji + YELLOW_BOX
+                counter_idx = counter_idx + 1
+            else:
+                emoji = emoji + WHITE_BOX
+                counter_idx = counter_idx + 1
+    return emoji
+
+def input_guess(correct_length: int) -> str:
+    """Given integer length of a guess, prompt the user for a guess of the correct length. When correct length guess is entered by user, function returns that guess"""
+    guess = input(f"Enter a {str(correct_length)} character word: ")
+    playing = True
+    while playing:
+        if (len(guess) != correct_length):
+            guess: str = input(f"That was not {str(correct_length)} letters! Try again: ")
+            playing = True
+        if (len(guess) == correct_length):
+            playing = False
+    return guess
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    SECRET: str = "codes"
+    turn_number: int = 1
+    running = True
+    while running == True and turn_number < 6:
+        print(f"=== Turn {str(turn_number)}/6 ===")
+        guess_word = input_guess(5)
+        print (emojified(guess_word, SECRET))
+        if (guess_word == SECRET):
+            print (f"You won in {str(turn_number)}/6 turns! ")
+            running = False
+        else:
+            turn_number = turn_number + 1
+            running = True
+    if (turn_number == 6 and guess_word != SECRET):
+        print ("X/6 - Sorry, try again tomorrow! ")
+        running = False
