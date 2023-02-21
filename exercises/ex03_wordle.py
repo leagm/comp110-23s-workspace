8fad1200874f371f7cb19e3ef03e37089e3f2664
Leah Morrissey <lmorrissey@unc.edu>
2023-02-20 22:04:34 -0500

    EX03 - Just need to add notes!

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,11 +2,16 @@
 
 __author__ = "730486771"
 
+# Lines 6-7 define the function contains_char function to search for a single character string in a string of any length 
 def contains_char(search_through: str, search_for: str) -> bool:
     """When given two strings, the first of any length, and the second a single character, return True if the second string is found anywhere in the first and return False if otherwise"""
+# asserted length of seacrh_for to be 1 so that the function is only searching for one character in the string being searched through 
     assert len(search_for) == 1
+# count_idx to keep track of which index the function body is examining
     count_idx: int = 0
+# elsewhere defined to keep function body running and serve as the return boolean value
     elsewhere = False
+# Lines 15-20 evaluate whether the single character search_for is found at any of the indices in the guessed string search_through using count_idx which will increase by 1 until it reaches the length of search_through, or until it finds an index where elsewhere is True. If it is found, the while loop ends and the value of elsewhere (True) is retured. If search_for is not found anywhere in search_through, the loop ends and elsewhere (False) is returned
     while elsewhere == False and count_idx < len(search_through):
         if (search_through[count_idx] == search_for):
             elsewhere = True
@@ -14,6 +19,7 @@ def contains_char(search_through: str, search_for: str) -> bool:
             count_idx = count_idx + 1
     return elsewhere
 
+# Lines 23-24 define the function emojified to, given two strings of equal length, one which is a secret and the other a guess, return a string of emojis that will codify according to the placement/existence of characters within guess words
 def emojified(guess_word: str, secret_word: str) -> str:
     """Given two strings of equal length, one which is a guess and the other which is a secret, return a string of emojis that will codify according to the placement/existence of characters within guess words"""
     assert len(guess_word) == len(secret_word)
@@ -52,16 +58,20 @@ def main() -> None:
     SECRET: str = "codes"
     turn_number: int = 1
     running = True
-    while running == True and turn_number < 6:
+    while running == True and turn_number <= 6:
         print(f"=== Turn {str(turn_number)}/6 ===")
-        guess_word = input_guess(5)
+        guess_word = input_guess(len(SECRET))
         print (emojified(guess_word, SECRET))
         if (guess_word == SECRET):
             print (f"You won in {str(turn_number)}/6 turns! ")
+            print()
             running = False
         else:
             turn_number = turn_number + 1
             running = True
-    if (turn_number == 6 and guess_word != SECRET):
+    if (turn_number > 6):
         print ("X/6 - Sorry, try again tomorrow! ")
-        running = False
+        print()
+
+if __name__ == "__main__":
+    main()
