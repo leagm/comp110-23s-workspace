dd4c834c6cb2ed890df8288c60a05f304f1ec752
Leah Morrissey <lmorrissey@unc.edu>
2023-02-28 13:19:55 -0500

    Worked on EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,4 +2,16 @@
 
 __author__ = "730486771"
 
-def all()
+def all(numbers_list: list, number: int) -> bool:
+    """Given a list of integers and an integer, return True if all integers in the list match the integer. Return False if otherwise."""
+    list_idx: int = 0
+    running: bool = True
+    while list_idx < int(len(numbers_list)) - 1 and running is True:
+        if number == numbers_list[list_idx]:
+            running = True
+            list_idx += 1
+        else:
+            running = False
+            return False
+    if running is True:
+        return True
