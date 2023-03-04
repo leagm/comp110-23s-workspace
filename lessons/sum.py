3ff5079816a6297bce01abea81a5dcd959f74def
Leah Morrissey <lmorrissey@unc.edu>
2023-03-03 22:47:03 -0500

    Did the CQs

--- lessons/sum.py
+++ lessons/sum.py
@@ -3,8 +3,7 @@
 def sum(xs: list[float]) -> float:
     """Return sum of all elements in xs"""
     sum_total: float = 0.0
-    idx: int = 0
-    while idx < len(xs):
+
+    for idx in range(0, len(xs)):
         sum_total += xs[idx]
-        idx += 1
     return sum_total
