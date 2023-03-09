5fa0ecba926a6a8e9d180b72c4fecb1bf3a74f1a
Leah Morrissey <lmorrissey@unc.edu>
2023-03-09 08:45:58 -0500

    Started on EX06

---
+++ exercises/cyoa.py
@@ -0,0 +1,12 @@
+"""EX06 - Create Your Own Adventure!"""
+
+__author__ = "730486771"
+
+def greet() -> None:
+    """Greets player with welcome message, provides context of game, asks player for their name."""
+
+
+def main() -> None:
+    """Entry point of the game and main game loop."""
+    points: int = 40
+    player: str = input("What is your name, hero?: ")
