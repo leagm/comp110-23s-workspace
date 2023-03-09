fe4f92925b189636abd243e27fa5d13a00ecffac
Leah Morrissey <lmorrissey@unc.edu>
2023-03-09 16:34:36 -0500

    Made some changes to EX06

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -8,19 +8,23 @@ WIZARD: str = "\U0001F9D9"
 def greet() -> None:
     """Greets player with welcome message, provides context of game, asks player for their name."""
     print("Welcome to MYTHICAL RESCUE! ")
-    will_you_help(input(f"{WIZARD}: Hello there! I am Grand-dalf the Great! I need your help, mysterious hero! A band of evil trolls have captured some of my mythical creatures and friends. Can you help me rescue them, hero? Y or N: "))
+    print((f"{WIZARD}: Hello there! I am Grand-dalf the Great! I need your help, mysterious hero! A band of evil trolls have captured some of my mythical creatures and friends. Can you help me rescue them, hero? "))
+    will_you_help(input("Y or N: "))
     playing: bool = True
     while playing == True:
         if will_you_help("Y") == True:
             print(f"{WIZARD}: Hurrah! Thank you hero! Let me explain how you will proceed. You will begin your rescue with 40 hero points. There is an evil troll guarding each mythical creature or being. To challenge a troll, you will roll for a random number. If your number is larger than the troll's number, you have defeated the troll and the troll's number will be added to your hero points. If not, then troll's number is subtracted from your hero points and you will roll again against the troll until you either defeat it or run out of hero points. Each creature or being that you rescue will add a point value to your hero points as you progress.")
             player: str = input("What is your name, hero?: ")
-        else: 
-            leave_rescue(input(f"{WIZARD}: That's okay, hero. Would you like to leave this rescue? Stay or Leave?: "))
-            if leave_rescue("Leave") == True:
+            print(f"{WIZARD}: Well met, {player}! Let us continue on this mythical quest!")
+            playing == False
+        if will_you_help("N") == False:
+            print((f"{WIZARD}: That's okay, hero. Would you like to leave this rescue? "))
+            leave_rescue(input("Y or N?: "))
+            if leave_rescue("Y") == True:
                 print(f"{WIZARD}: Farewell, hero! I hope we meet again!")
                 playing == False
                 quit()
-            else:
+            if leave_rescue("N"):
                 playing == True
                 will_you_help("Y") == True
                 
@@ -29,7 +33,7 @@ def will_you_help(response: str) -> bool:
     """Wizard asks if you will help, you respond either Yes or No."""
     if response == "Y":
         return True
-    else: 
+    if response == "N": 
         return False
 
 
