80aa6a572d50b9ce6acb50ccb83f18981da817b3
Leah Morrissey <lmorrissey@unc.edu>
2023-03-09 11:38:32 -0500

    Finished CQ04

--- lessons/zip.py
+++ lessons/zip.py
@@ -2,14 +2,14 @@
 
 def zip(list1: list[str], list2: list[int]) -> dict[str, int]:
     """Given and list of strings and a list of ints, return a dictionary where the keys are the items from the first list and the values are the items from the second list."""
-    output_dictionary: dict[str, int] = dict()
-    y_idx: int = 0
-    y: str = list2[y_idx]
+    output_dictionary: dict[str, int] = {}
+    integer_idx: int = 0
     count_idx: int = 0
     if len(list1) != len(list2) or len(list1) == 0 or len(list2) == 0:
         return output_dictionary
     else:
-        while y_idx < (len(list2) - 1) and count_idx < (len(list1) - 1):
-            list1[count_idx] = y[y_idx]
-            y_idx += 1
+        while integer_idx < (len(list2)) and count_idx < (len(list1)):
+            output_dictionary[list1[count_idx]] = list2[integer_idx]
+            integer_idx += 1
+            count_idx += 1
     return output_dictionary
