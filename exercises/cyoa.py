cbeeb4db65c219aa0d96937daf9775fddf87f051
Leah Morrissey <lmorrissey@unc.edu>
2023-03-16 18:05:49 -0400

    Made some changes to EX06

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -9,38 +9,37 @@ def greet() -> None:
     """Greets player with welcome message, provides context of game, asks player for their name."""
     print("Welcome to MYTHICAL RESCUE! ")
     print((f"{WIZARD}: Hello there! I am Grand-dalf the Great! I need your help, mysterious hero! A band of evil trolls have captured some of my mythical creatures and friends. Can you help me rescue them, hero? "))
-    will_you_help(input("Y"))
+    will_you_help = input("Y or N: ")
     playing = True
     while playing:
-        if (will_you_help(input("Y"))) == True:
+        if will_you_help == "Y":
             print(f"{WIZARD}: Hurrah! Thank you hero! Let me explain how you will proceed. You will begin your rescue with 40 hero points. There are 7 evil trolls for you to defeat, and 7 creatures to save. There is an evil troll guarding each mythical creature or being. To challenge a troll, you will roll for a random number. If your number is larger than the troll's number, you have defeated the troll and the troll's number will be added to your hero points. If not, then troll's number is subtracted from your hero points and you will roll again against the troll until you either defeat it or run out of hero points. Each creature or being that you rescue will add a point value to your hero points as you progress.")
             player: str = input("What is your name, hero?: ")
             print(f"{WIZARD}: Well met, {player}! Let us continue on this mythical quest!")
             playing = False
-        else:
+        elif will_you_help == "N":
             print((f"{WIZARD}: That's okay, hero. Would you like to leave this rescue? "))
-            leave_rescue(input("Y or N?: "))
-            if (leave_rescue(input("Y or N?: "))) == True:
+            if leave_rescue(input("Y or N?: ")) == True:
                 print(f"{WIZARD}: Farewell, hero! I hope we meet again!")
                 playing = False
                 quit()
+            elif leave_rescue(input("Y or N?: ")) == False:
+                print(f"{WIZARD}: Hurrah! Thank you hero! Let me explain how you will proceed. You will begin your rescue with 40 hero points. There are 7 evil trolls for you to defeat, and 7 creatures to save. There is an evil troll guarding each mythical creature or being. To challenge a troll, you will roll for a random number. If your number is larger than the troll's number, you have defeated the troll and the troll's number will be added to your hero points. If not, then troll's number is subtracted from your hero points and you will roll again against the troll until you either defeat it or run out of hero points. Each creature or being that you rescue will add a point value to your hero points as you progress.")
+                player: str = input("What is your name, hero?: ")
+                print(f"{WIZARD}: Well met, {player}! Let us continue on this mythical quest!")
+                playing = False
             else:
-                playing = True
-                (will_you_help(input("Y or N: "))) == True
-                
-
-def will_you_help(response: str) -> bool:
-    """Wizard asks if you will help, you respond either Yes or No."""
-    if response == "Y":
-        return True
+                print(input("Please enter Y or N: "))
+        else:
+            print(input("Please enter Y or N: "))
 
 
 def leave_rescue(response: str) -> bool:
     """If you responded that you did not want to help the Wizard, you are given the option to leave the game. If you choose to stay,"""
-    if response == "Stay":
-        return False
-    else:
+    if response == "Y":
         return True
+    if response == "N":
+        return False
 
 
 def main() -> None:
