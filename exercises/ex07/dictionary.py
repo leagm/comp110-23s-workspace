8a97c9efd3bb34c0251a3b4e4ad706a631d137e0
Leah Morrissey <lmorrissey@unc.edu>
2023-03-28 09:55:37 -0400

    Worked on EX07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -9,4 +9,5 @@ def invert(my_dict: dict[str, str]) -> dict[str, str]:
     for x in my_dict:
         x = my_dict[x] 
         my_dict[x] = x
-    
+        new_dict.append(my_dict)
+    return new_dict
