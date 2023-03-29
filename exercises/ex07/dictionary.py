47d9056f3c2320693f3e9f3d47262052d7f32d34
Leah Morrissey <lmorrissey@unc.edu>
2023-03-29 17:18:31 -0400

    Worked on EX07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -5,8 +5,12 @@ __author__ = "730486771"
 
 def invert(my_dict: dict[str, str]) -> dict[str, str]:
     """Given a dict[str, str] invert the keys and values of the given dictionary."""
-    new_dict: dict[str, str] = []
-    for x in my_dict:
-        x = my_dict[x] 
-    new_dict = my_dict
-    return new_dict
+    inverted_dict: dict[str, str] = {}
+    
+    for k, v in my_dict:
+        if k == k or v == v:
+            raise KeyError("Key Error")
+    for keys in my_dict:
+       value = my_dict[keys]
+       inverted_dict[value] = keys
+    return inverted_dict
--- exercises/ex07/dictionary_tests.py
+++ exercises/ex07/dictionary_tests.py
@@ -1,3 +1,5 @@
 """Unit tests for EX07 functions."""
 
 __author__ = "730486771"
+
+from exercises.ex07.dictionary import invert
