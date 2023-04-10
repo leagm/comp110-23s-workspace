725e85a8cfd82a751c0f39e91063ee77a2c3293b
Leah Morrissey <lmorrissey@unc.edu>
2023-04-09 23:22:21 -0400

    Worked on EX08

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -39,13 +39,47 @@ def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
 def head(table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
     """Produces a new column-based table with the first ___(number) rows of data for each column."""
     return_dict: dict[str, list[str]] = {}
-    i = 0
 
     for column in table:
         return_vals: list[str] = []
-        for item in column:
-            while i in range(n + 1):
+        for item in (table[column]):
+            while item in range(1, n + 1):
                 return_vals.append(item)
         return_dict[column] = return_vals
     return return_dict
+        
+
+def select(table: dict[str, list[str]], col_names: list[str]) -> dict[str, list[str]]:
+    """Return table with only certain selected columns of data."""
+    return_dict: dict[str, list[str]] = {}
+
+    for column in col_names:
+        return_dict[column] = table[column]
+    return return_dict
     
+
+def concat(table1: dict[str, list[str]], table2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce new column-based data table made from two combined tables."""
+    new_dict: dict[str, list[str]] = {}
+
+    for column in table1:
+        new_dict[column] = table1[column]
+    for column in table2:
+        if column in new_dict:
+            new_dict[column] += table2[column]
+        else:
+            new_dict[column] = table2[column]
+    return new_dict
+
+
+def count(my_list: list[str]) -> dict[str, int]:
+    """Given a list[str], this function will produce a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
+    # Created an empty dictionary to add my eventual result to
+    return_dict: dict[str, int] = {}
+    # Created a loop that will go through and check if each value in the list is in the new dictionary. If it is, then the value associated with it will increase by 1. If not, it will be added as a key to the dictionary and as it is a newly discovered element, its value will be equal to one since it is the first. The completed dictionary is returned after entire list has been evaluated
+    for value in my_list:
+        if value in return_dict:
+            return_dict[value] += 1
+        else:
+            return_dict[value] = 1
+    return return_dict
