4d872fcdf36347d84948320201db06a0cf167eb2
Leah Morrissey <lmorrissey@unc.edu>
2023-04-25 22:30:01 -0400

    Worked on EX010

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -34,5 +34,24 @@ class Simpy:
         
         return None
 
-    def arange (self, start: float, stop: float, simpy_self: float):
+    def arange (self, start: float, stop: float, step: float):
         """Fills in object values attribute with a range of float values."""
+        range_values: list[float] = []
+        n_start: int = int(start)
+        n_stop: int = int(stop)
+        n_step: int = int(step)
+        step != 1.0 or 1
+        for _ in range(n_start, n_stop + 1, n_step):
+            range_values.append(_)
+        for values in range_values:
+            float(values)
+        self.values = range_values
+
+        return None
+    
+    def sum (self) -> float:
+        """Computes the sum of all values in the object values attribute."""
+        return_val = sum(self.values)
+        
+        return return_val
+    
